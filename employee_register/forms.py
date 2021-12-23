f53da6b93e34c356c6369fb0e0b2e0a889b8ff18
Muhammad Irfan Aslam <engr.irfan641@gmail.com>
2021-12-23 12:34:28 +0500

    update compose file for database

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -5,4 +5,4 @@ from .models import Employee
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ('fullname', 'emp_code', 'mobile', 'position' )
