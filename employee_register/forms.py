0c9e77bd5662ad3720c1c699c8e6648b1aee67ac
Muhammad Irfan Aslam <engr.irfan641@gmail.com>
2021-12-23 20:24:15 +0500

    Register and List Employee Record

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -5,4 +5,15 @@ from .models import Employee
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = ('fullname', 'emp_code', 'mobile', 'position' )
+        fields = ('fullname', 'mobile', 'emp_code', 'position')
+        labels = {
+            'fullname': 'Full Name',
+            'position': 'Desigination',
+            'emp_code': 'Employee Code',
+            'mobile': 'Mobile Number'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = "Select"
+        self.fields['emp_code'].required = False
